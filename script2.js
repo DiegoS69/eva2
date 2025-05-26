e5c30fb6345143c194c153266906e82d15e8f3f9
DiegoS69 <diego.i.sotomayor.s@gmail.com>
2025-05-26 14:11:29 -0400

    primer commit

---
+++ script2.js
@@ -0,0 +1,17 @@
+//validar nombre, debe contener solo letras y no ser vacio
+//validar edad: Debe ser mayor o igual a 18 y menor a 100
+
+function validar(){
+    let nombre = document.getElementById("nombre")
+    let vNombre = nombre.value
+    let edad = document.getElementById("edad")
+    let vEdad = edad.value
+
+    let eNombre = document.getElementById("Hay un error en su nombre")
+    let eEdad = document.getElementById("Hay un error en su edad")
+
+    let validarNombre = validarnombre (nombre, vNombre, eNombre)
+    let validarEdad = validaredad (edad, vEdad, eEdad)
+
+
+}
